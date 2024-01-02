904a894a5202637635f9ed466226cfede3d4fb68
AshArce <arcebuchejohnashley@gmail.com>
2024-01-03 06:14:54 +0800

    done login

--- controllers/userController.js
+++ controllers/userController.js
@@ -30,24 +30,29 @@ async function createUser(req, res, next) {
 
 async function loginUser(req, res, next) {
   const { username, password } = req.body;
-  const user = await User.findOne({ username });
-  const passwordCorrect =
-    user === null ? false : await bcrypt.compare(password, user.passwordHash);
+  try {
+    const user = await User.findOne({ username });
+    const passwordCorrect =
+      user === null ? false : await bcrypt.compare(password, user.passwordHash);
 
-  if (!(user && passwordCorrect)) {
-    return res.status(401).json({ error: "Invalid username or passsword" });
+    if (!(user && passwordCorrect)) {
+      return res.status(401).json({ error: "Invalid username or password" });
+    }
+
+    const userForToken = {
+      username: user.username,
+      id: user._id,
+    };
+
+    const token = jwt.sign(userForToken, config.JWT_SECRET);
+    console.log("Generated token:", token);
+
+    return res
+      .status(200)
+      .json({ token, username: user.username, name: user.name });
+  } catch (error) {
+    next(error);
   }
-
-  const userForToken = {
-    username: user.usernam,
-    id: user._id,
-  };
-
-  const token = jwt.sign(userForToken, config.JWT_SECRET);
-
-  return res
-    .status(200)
-    .json({ token, username: user.username, name: user.name });
 }
 export default {
   createUser,

1b1285af2e1fccf6adb6fcc0564206e68954ac42
AshArce <arcebuchejohnashley@gmail.com>
2024-01-20 06:03:24 +0800

    disst

--- middlewares/unknownEndpoint.js
+++ middlewares/unknownEndpoint.js
@@ -2,3 +2,4 @@ export default function unknownEndpoint(req, res) {
   console.log(`Unknown endpoint: ${req.method} ${req.url}`);
   return res.status(404).send({ error: "unknown endpoint" });
 }
+//dsaas
